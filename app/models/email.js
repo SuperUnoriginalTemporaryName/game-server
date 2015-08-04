c950b681eae9aabdb7f13b139ea73cd68b46876d
Connor Bode <connor.bode@gmail.com>
2015-08-04 18:46:40 -0400

    update email and user models

--- app/models/email.js
+++ app/models/email.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Email.belongsTo(models.User, { foreignKey: 'UserId'})
       }
     }
   });
--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {}, {
     classMethods: {
       associate: function(models) {
-        User.hasMany(models.Email, { as: 'emails' });
+        User.hasMany(models.Email, { foreignKey: 'UserId' });
       }
     }
   });
