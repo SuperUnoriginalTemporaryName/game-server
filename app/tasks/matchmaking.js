d0bd8060c350a4228c4f8e91ec74e21a075c08e7
Connor Bode <connor.bode@gmail.com>
2015-08-17 20:45:37 -0400

    fixing up matchmaking

--- app/tasks/matchmaking.js
+++ app/tasks/matchmaking.js
@@ -4,11 +4,18 @@ var matchmaking = {
     return new Promise((resolve, reject) => {
       db.sequelize.transaction(t => {
         return db.Matchmaking.findOne({
-          
+          matched: false,
+          cancelled: false
         }, {
           transaction: t
         }).then(matchmaking => {
-
+          if (!matchmaking) {
+            return db.Matchmaking.create({
+              userOne: user.id
+            });
+          } else {
+            
+          }
         });
       });
     });
